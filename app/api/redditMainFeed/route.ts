dd250bedfe0baf48e75e427cba328250c31b8bed
fuser <vnovodid@gmail.com>
2025-04-06 17:31:03 +0300

    fix: remove cache handling for route.ts for reddit feed

--- app/api/redditMainFeed/route.ts
+++ app/api/redditMainFeed/route.ts
@@ -14,8 +14,7 @@ export async function GET() {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
                 'Accept': 'application/json',
-            },
-            next: { revalidate: 0 },
+            }
         });
 
         console.log('[API] Fetch response status:', response.status);
